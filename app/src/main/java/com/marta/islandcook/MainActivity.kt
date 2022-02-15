94d1fe412633851a3594eacd634ec71335a38fc8
Marta Molina <marta.molina@live.u-tad.com>
2022-02-15 20:05:19 +0100

    Navigation bar working

--- app/src/main/java/com/marta/islandcook/MainActivity.kt
+++ app/src/main/java/com/marta/islandcook/MainActivity.kt
@@ -2,11 +2,19 @@ package com.marta.islandcook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.marta.islandcook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_IslandCook)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //Set Up NavigationBar
+        val navController = findNavController(R.id.nav_host_fragment)
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
