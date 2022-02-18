6624cc7323fe0d8b2f2fd1e6a28693f12e98b9a5
Marta Molina <marta.molina@live.u-tad.com>
2022-02-19 00:36:29 +0100

    DetailView Chips and cleaned code a little bit

--- app/src/main/java/com/marta/islandcook/usecases/recipes/RecipeDetailFragment.kt
+++ app/src/main/java/com/marta/islandcook/usecases/recipes/RecipeDetailFragment.kt
@@ -1,119 +1,169 @@
 package com.marta.islandcook.usecases.recipes
 
+import android.content.res.ColorStateList
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.compose.ui.graphics.Color
+import androidx.core.content.ContextCompat
+import androidx.core.view.marginLeft
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
+import com.google.android.material.chip.Chip
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.marta.islandcook.R
-import com.marta.islandcook.databinding.FragmentHomeBinding
 import com.marta.islandcook.databinding.FragmentRecipeDetailBinding
 import com.marta.islandcook.model.response.RecipeResponse
 import com.marta.islandcook.provider.api.NetworkManagerRecipesAPI
-import com.marta.islandcook.provider.api.NetworkService
-import com.marta.islandcook.usecases.common.HomeListAdapter
+import com.marta.islandcook.provider.db.IslandCook_Database
+import com.marta.islandcook.provider.db.entities.Recipies
 import com.marta.islandcook.utils.imageUrl
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 
-class RecipeDetailFragment: Fragment() {
+class RecipeDetailFragment : Fragment() {
+    //TODO Add Shimmer
     private var recipe: RecipeResponse? = null
     private val args: RecipeDetailFragmentArgs by navArgs()
     private var _binding: FragmentRecipeDetailBinding? = null
     private val binding
         get() = _binding!!
+    private val likedRecipes: MutableList<String> = mutableListOf()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentRecipeDetailBinding.inflate(inflater, container, false)
         return binding.root
-        //TODO _binding = null
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewLifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
+            getLikedRecipes()
             requestData(args.recipeId)
+            changeIconLike(isItLiked())
             withContext(Dispatchers.Main) {
                 populateUI(recipe!!)
             }
         }
     }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
+    //------------------------ UI
 
     private suspend fun showError() = withContext(Dispatchers.Main) {
         MaterialAlertDialogBuilder(requireContext())
             .setTitle("Error")
-            .setMessage("Error de conexión.\nIntántalo de nuevo más tarde")
+            .setMessage("Error de conexión.\nInténtalo de nuevo más tarde")
             .setPositiveButton("Okay, Polisha") { dialog, which ->
                 // Respond to positive button press
             }
             .show()
     }
 
-    private suspend fun requestData(recipeId: String) {
-            try {
-                recipe = NetworkManagerRecipesAPI.service.getRecipeById(recipeId)
-            }
-            catch (error: Exception) {
-                showError()
-            }
-    }
 
-    private suspend fun populateUI(recipeResponse: RecipeResponse) {
-        var liked: Boolean
+    private fun populateUI(recipeResponse: RecipeResponse) {
         var stringIngredients = ""
         var stringSteps = ""
         var stringTags = ""
         recipeResponse?.let {
-            binding.tvNameRecipe.text = it.name
+            binding.tvNameRecipeDetail.text = it.name
             binding.ivImgRecipe.imageUrl(it.pictureUrl)
-            binding.tvAuthor.text = it.author
+            binding.tvAuthorDetail.text = it.author
             recipeResponse.ingredients.forEach {
-                stringIngredients += "${it.name} ${it.amount} \n"
+                stringIngredients += "- ${it.name}: ${it.amount} \n\n"
             }
             recipeResponse.steps.forEach {
-                stringSteps += "${it.length} \n"
+                stringSteps += "- ${it} \n\n"
             }
-            binding.tvIngredients.text = stringIngredients
-            binding.tvSteps.text = it.steps.toString()
+            binding.tvIngredientsDetail.text = stringIngredients
+            binding.tvStepsDetail.text = stringSteps
             binding.ibLikeDetail.setOnClickListener {
-                if (isliked(liked = true)) {
-                    liked = false
-                } else {
-                    liked = true
-                }
+                likeDislike(recipeResponse)
+            }
+            recipeResponse.tags.forEach {
+                addChip(it)
             }
         }
     }
 
-    private fun isliked(liked: Boolean): Boolean {
-        if (liked) {
-            binding.ibLikeDetail.setImageResource(R.drawable.like_detail_filled)
+    private fun addChip(chipText: String) {
+        val chip = Chip(requireContext())
+        chip.text = chipText
+        chip.isClickable = false
+        chip.chipBackgroundColor = ColorStateList.valueOf(ContextCompat.getColor(requireContext(), R.color.tertiary90))
+        chip.chipStrokeColor = ColorStateList.valueOf(ContextCompat.getColor(requireContext(), R.color.tertiary90))
+        binding.chipTagsDetail.addView(chip as View)
+    }
+
+    private suspend fun changeIconLike(liked: Boolean) = withContext(Dispatchers.Main) {
+        if (!liked) {
+            binding.ibLikeDetail.setImageResource(R.drawable.ic_baseline_favorite_border_35)
         } else {
-            binding.ibLikeDetail.setImageResource(R.drawable.like_xavi)
+            binding.ibLikeDetail.setImageResource(R.drawable.ic_baseline_favorite_35)
         }
-        return liked
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
     //------------------------ DB
+    private suspend fun getLikedRecipes() {
+        likedRecipes.clear()
+        val savedRecipes =
+            IslandCook_Database.getInstance(requireContext()).recipiesDao().findAllRecipies()
+        savedRecipes.forEach { likedRecipes.add(it.recipeId) }
+    }
 
+    private fun isItLiked(): Boolean {
+        return likedRecipes.contains(args.recipeId)
+    }
+
+    private fun likeDislike(item: RecipeResponse) {
+        viewLifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
+            if (isItLiked()) {
+                dislike()
+                likedRecipes.remove(args.recipeId)
+                changeIconLike(false)
+            } else {
+                saveRecipe(item)
+                likedRecipes.add(args.recipeId)
+                changeIconLike(true)
+            }
+        }
+    }
+
+    private suspend fun dislike() {
+        IslandCook_Database.getInstance(requireContext()).recipiesDao()
+            .deleteRecipieById(args.recipeId)
+    }
+
+    private suspend fun saveRecipe(item: RecipeResponse) {
+        IslandCook_Database.getInstance(requireContext()).recipiesDao().insertRecipies(
+            Recipies(
+                item.id,
+                item.name,
+                item.pictureUrl,
+                item.difficulty,
+                item.author,
+                false
+            )
+        )
+        likedRecipes.add(item.id)
+    }
+
+    //------------------------ API
+    private suspend fun requestData(recipeId: String) {
+        try {
+            recipe = NetworkManagerRecipesAPI.service.getRecipeById(recipeId)
+        } catch (error: Exception) {
+            showError()
+        }
+    }
     //------------------------ NAVIGATION
 }
