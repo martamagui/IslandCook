0a2d4a0dff8aebfa835cb55270370c14cbd1d47c
Aracely <cely14052000@gmail.com>
2022-02-20 12:32:20 +0100

    Add View do it let's gou

--- app/src/main/java/com/marta/islandcook/model/body/RecipeBody.kt
+++ app/src/main/java/com/marta/islandcook/model/body/RecipeBody.kt
@@ -4,13 +4,13 @@ import com.google.gson.annotations.SerializedName
 import com.marta.islandcook.model.response.Ingredient
 
 class RecipeBody(
-    val name: String,
-    val ingredients: List<Ingredient>,
-    val steps: List<String>,
-    val pictureUrl: String,
-    val difficulty: String,
-    val author: String,
-    val tags: List<String>
+    var name: String,
+    var steps: List<String>,
+    var ingredients: List<Ingredient>,
+    var picture_url: String,
+    var difficulty: String,
+    var author: String,
+    var tags: List<String>
 
 )
 
